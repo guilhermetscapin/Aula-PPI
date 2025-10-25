c1aad3cbf6af1c021fc0a22373c54c8ea95874a1
scapin <guilhermescapin.dev@gmail.com>
2025-10-25 13:20:57 -0300

    Base do Trabalho de Reajuste

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+
+const host = '0.0.0.0';
+const porta = 3000;
+
+const server = express(); //oferecendo ao desenvolvedor um servidor HTTP de modo expresso.
+
+//rechear o servidor com funcionalidades
+
+server.get('/', (req, resp) => {
+    resp.send(`
+        <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Reajuste de Salário</title>
+            </head>
+            <body>
+                <h1>Olá, mundo!</h1>
+            </body>
+            </html>
+        `);
+});
+
+server.listen(porta, host, () => {
+    console.log(`Servidor escutando em http://${host}:${porta}`);
+});
+
